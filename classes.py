f408e134a6d0e2e2f802584533cbd93cef74ea92
Dennis Pražák <prazak.dennis@gmail.com>
2018-10-16 11:52:29 +0200

    Pridat funkci serad do Batohu

--- classes.py
+++ classes.py
@@ -3,6 +3,8 @@ class Batoh:
         self.obsah = []
     def pridej(self, objekt):
         self.obsah.append(objekt)
+    def serad(self):
+        self.obsah = sorted(self.obsah)
 
 class Person:
     def __init__(self, batoh):
@@ -12,4 +14,6 @@ ruksak = Batoh()
 ruksak.pridej("tuzka")
 ruksak.pridej("papir")
 p = Person(ruksak)
-p.batoh.obsah
+print(p.batoh.obsah)
+p.batoh.serad()
+print(p.batoh.obsah)
