91d2945cd32f066a000e341c1cd2a4d6518b48af
Dennis Pražák <prazak.dennis@gmail.com>
2018-11-09 09:20:12 +0100

    Fix remainder

--- caesar.py
+++ caesar.py
@@ -1,15 +1,15 @@
 def caesar(vstup, posun):
     vysledek = ''
-    posun %= 25
+    posun %= 26
     # A-Z -- 65-90
     # a-z -- 97-122
     for i in range(len(vstup)):
         o = ord(vstup[i])
         if o >= 65 and o <= 90:
-            o = 65 + (((o-65) + posun) % 25)
+            o = 65 + (((o-65) + posun) % 26)
         elif o >= 97 and o <= 122:
-            o = 97 + (((o-97) + posun) % 25)
+            o = 97 + (((o-97) + posun) % 26)
         vysledek += chr(o)
     return vysledek
 
-print(caesar('ahojz', 5))
+print(caesar('Ahoj, svete, zzzz', 5))
